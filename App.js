11fd8ea968aa974948450ba120a13d098bce185c
Christoph <christoph-85@hotmail.com>
2019-11-12 15:15:18 +0800

    welcome screen shell with grid
    added the react-navigation dependancy

--- App.js
+++ App.js
@@ -6,7 +6,10 @@
  * @flow
  */
 
-import React from 'react';
+import React, {
+  useState
+} from 'react';
+
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,6 +17,7 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
 } from 'react-native';
 
 import {
@@ -24,90 +28,169 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+      <View style={styles.app}>
+        <View style={styles.welcomeScreen}>
+          {/* buttons logo */}
+          <View style={styles.bunningsLogo}>
+            {/* Temp for showing state change */}
+            <Text style={{textAlign:'center', marginTop:'auto', marginBottom:'auto'}}>{counter}</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
+          {/* welcome title container */}
+          <View style={styles.welcomeTextContainer}>
+            <Text style={styles.welcomeTextTitle}>CARD ONLY</Text>
+            <Text style={styles.welcomeTextSubTitleContainer}>
+              <Text style={styles.welcomeTextSubTitle}>Cash payments and cash out </Text>
+              <Text style={styles.welcomeTextSubTitleNotAvailable}>not available</Text>
+              <Text style={styles.welcomeTextSubTitle}> at Self Checkout</Text></Text>
+          </View>
+          {/* button Container */}
+          <View style={styles.welcomeButtonContainer}>
+            <View style={{flex:0.35, alignSelf:'center', borderWidth:1, borderColor:'pink', borderRadius:10}}>
+              <Button title='Tap Screen Or Scan To Start' onPress={()=>{setCounter(counter+1)}}></Button>
+            </View>
+          </View>
+          <View style={styles.welcomeFooter}>
+            <View style={styles.welcomeSupportButtonArea}>
+              {/* support button component */}
+              <View style={styles.welcomeSupportButtonContainer}>
+                <View style={styles.supportButton}/>
+                <Text style={styles.supportButtonText}>English</Text>
+              </View>
+              {/* support button component */}
+              <View style={styles.welcomeSupportButtonContainer}>
+                <View style={styles.supportButton}/>
+                <Text style={styles.supportButtonText}>Accessibility</Text>
+              </View>
+            </View>
+            <View style={styles.welcomeAcceptedPaymentTypes}>
+              <View style={{flex:0.5, flexDirection:'row', height: 50, borderWidth:1, borderColor:'pink'}}>
+                 {/* PaymentType title */}
+                 <Text style={{color:'#4D4D4D', fontWeight:'bold', alignSelf:'flex-end', borderWidth:1, borderColor:'pink'}}>Accepted Payment Types</Text>
+              </View>
+              <View style={{flex:0.5, flexDirection:'row', borderWidth:1, borderColor:'pink'}}>
+                  {/* PaymentType icons */}
+                  <View style={{width: 75, height: 40, marginTop:'auto', marginBottom:'auto', marginRight:10, backgroundColor:'lightgray'}}></View>
+                  <View style={{width: 75, height: 40, marginTop:'auto', marginBottom:'auto', marginRight:10, backgroundColor:'lightgray'}}></View>
+                  <View style={{width: 75, height: 40, marginTop:'auto', marginBottom:'auto', marginRight:10, backgroundColor:'lightgray'}}></View>
+                  <View style={{width: 75, height: 40, marginTop:'auto', marginBottom:'auto', marginRight:10, backgroundColor:'lightgray'}}></View>
+                  <View style={{width: 75, height: 40, marginTop:'auto', marginBottom:'auto', marginRight:10, backgroundColor:'lightgray'}}></View>
+                  <View style={{width: 75, height: 40, marginTop:'auto', marginBottom:'auto', marginRight:10, backgroundColor:'lightgray'}}></View>
+                  <View style={{width: 75, height: 40, marginTop:'auto', marginBottom:'auto', marginRight:10, backgroundColor:'lightgray'}}></View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  app:{
+    flex:1,
+    alignContent:'stretch',
+    backgroundColor:'gray'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  welcomeScreen:{
+    flex:1,
+    margin: 10,
+    backgroundColor: '#0D5257',
+    
+    borderWidth: 1,
+    borderColor: 'pink'
   },
-  body: {
-    backgroundColor: Colors.white,
+  bunningsLogo: {
+    margin: 20,
+    width: 175,
+    height:50,
+    backgroundColor: '#ffff',
+
+    borderWidth: 1,
+    borderColor: 'pink'
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  welcomeTextContainer: {
+    flex:1,
+
+    borderWidth: 1,
+    borderColor: 'pink'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  welcomeTextSubTitleContainer:{
+    textAlign:'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  welcomeTextTitle:{
+    color: '#fff',
+    textAlign:'center',
+    fontSize: 88,
+
+    borderWidth: 1,
+    borderColor: 'pink'
   },
-  highlight: {
-    fontWeight: '700',
+  welcomeTextSubTitle:{
+    color: '#fff',
+    textAlign:'center',
+    letterSpacing: 1.5,
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  welcomeTextSubTitleNotAvailable:{
+    color: '#F2AE3D',
+    textAlign:'center',
+    letterSpacing: 1.5,
+  },
+  welcomeButtonContainer:{
+    flex: 1,
+    flexDirection:'row',
+    alignSelf:'center',
+    height: 50,
+    borderWidth: 1,
+    borderColor: 'pink'
+  },
+  welcomeFooter:{
+    flex:1,
+    flexDirection:'row',
+    backgroundColor: '#fff',
+
+    borderWidth: 1,
+    borderColor: 'pink'
+  },
+  welcomeSupportButtonArea:{
+    flex:0.5,
+    flexDirection:'row',
+    alignSelf:'flex-end',
+    margin: 10,
+
+    borderWidth: 1,
+    borderColor: 'pink'
+  },
+  welcomeSupportButtonContainer:{
+    alignSelf:'flex-start',
+    borderWidth: 1,
+    borderColor: 'pink',
+    paddingRight: 10,
+    paddingLeft: 10,
+  },
+  supportButton:{
+    alignSelf:'flex-start',
+    width: 100,
+    height: 100,
+    backgroundColor: 'lightblue',
+    borderRadius: 100,
+
+    borderWidth: 1,
+    borderColor: 'pink'
+  },
+  supportButtonText:{
+    color:'#0D5257',
+    textAlign:'center'
+  },
+  welcomeAcceptedPaymentTypes: {
+    flex:0.5,
+
+    borderWidth: 1,
+    borderColor: 'pink'
   },
 });
 
